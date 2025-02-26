ac6c353f897dc07fbad275dedc3048be1ddedab1
222124 <222124@WINMAC6>
2025-02-26 15:37:43 +0300

    Добавьте файлы проекта.

---
+++ Alina_Game/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alina_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
